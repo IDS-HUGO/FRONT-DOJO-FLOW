734b9fd3d955307b615c9230cf71961a215cda28
AlessandraU03 <233303@ids.upchiapas.edu.mx>
2026-04-17 10:55:44 -0600

    Configuraciones

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/', // 🔥 IMPORTANTE
   plugins: [react()],
   server: {
     port: 3000,
@@ -10,3 +11,4 @@ export default defineConfig({
     'process.env': process.env,
   },
 })
+
